1a4e146131fb3f48cc5d2ace9704f7d403c6863f
Christian Aguero <christian.aguero@viridian.local>
2021-07-28 00:25:04 -0300

    pura mierda

--- components/effect.tsx
+++ components/effect.tsx
@@ -1,32 +1,71 @@
 import gsap from "gsap";
-import React, { Fragment, useLayoutEffect, useRef } from "react";
+import React, { useLayoutEffect, useRef, useState } from "react";
 
-function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
+// function numberWithCommas(x) {
+//   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+// }
 
 export const Effect = ({ children }) => {
-  const childrenRef = useRef([]);
+  const outRef = useRef(null);
+  const out2Ref = useRef(null);
+
+  // const cloneChildren = (children, rref) => {
+  //   return (
+  //     <>
+  //       {React.Children.map(children, (child, index) =>
+  //         React.cloneElement(child, {
+  //           ref: ref => (rref.current[index] = ref),
+  //         })
+  //       )}
+  //     </>
+  //   );
+  // };
+
+  const [prevHTML, setPrevHTML] = useState(null);
+  const [out, setOut] = useState(null);
+  const [out2, setOut2] = useState(null);
 
   useLayoutEffect(() => {
-    const items = childrenRef.current.map(element => element);
-    const tl = gsap.from(items, {
-      x: "-=800%",
-      duration: 1,
-      ease: "expo",
-    });
+    setOut(<div ref={outRef}>{children.props.children}</div>);
+    setOut2(<div ref={out2Ref}>{prevHTML}</div>);
+    setPrevHTML(children.props.children);
+
+    const enter = gsap.fromTo(
+      outRef.current,
+      {
+        x: "-=110px",
+        opacity: 0,
+      },
+      {
+        duration: 0.5,
+        ease: "linear",
+        x: "center",
+        position: "absolute",
+        opacity: 1,
+      }
+    );
+    const leave = gsap
+      .fromTo(
+        out2Ref.current,
+        {
+          x: "center",
+          ease: "linear",
+        },
+        {
+          duration: 0.5,
+          x: "+=130px",
+        }
+      )
+      .restart();
     return () => {
       //tl.kill();
     };
-  }, [children]);
+  }, [children.props]);
 
   return (
     <>
-      {React.Children.map(children, (child, index) =>
-        React.cloneElement(child, {
-          ref: ref => (childrenRef.current[index] = ref),
-        })
-      )}
+      {out}
+      {out2}
     </>
   );
 };
